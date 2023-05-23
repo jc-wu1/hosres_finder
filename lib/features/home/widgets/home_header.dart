e089fb4e39d0193e5ffd2b17d02a6d650d57c8d7
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2023-05-23 22:22:42 +0700

    add change location feature

--- lib/features/home/widgets/home_header.dart
+++ lib/features/home/widgets/home_header.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hosres_finder/features/fav_places/display/bloc/fav_places_bloc.dart';
 import 'package:hosres_finder/features/fav_places/display/fav_places_page.dart';
 import 'package:hosres_finder/features/geocoder/bloc/geocoder_bloc.dart';
+import 'package:hosres_finder/features/geolocator/bloc/geolocator_bloc.dart';
+import 'package:hosres_finder/features/map/map_page.dart';
 
 class HomeHeader extends StatelessWidget {
   const HomeHeader({
@@ -53,7 +58,38 @@ class HomeHeader extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        var state = context.read<GeolocatorBloc>().state;
+                        double lat = state.position!.latitude;
+                        double long = state.position!.longitude;
+
+                        Navigator.of(context)
+                            .push(
+                          MaterialPageRoute<LatLng>(
+                            builder: (context) => MapPage(
+                              position: LatLng(lat, long),
+                            ),
+                          ),
+                        )
+                            .then((value) {
+                          if (value != null) {
+                            context.read<GeolocatorBloc>().add(
+                                  GeolocatorUpdated(
+                                    Position(
+                                      longitude: value.longitude,
+                                      latitude: value.latitude,
+                                      timestamp: DateTime.now(),
+                                      accuracy: 0,
+                                      altitude: 0,
+                                      heading: 0,
+                                      speed: 0,
+                                      speedAccuracy: 0,
+                                    ),
+                                  ),
+                                );
+                          }
+                        });
+                      },
                       child: Text(
                         state.geocoder.results!.first.formattedAddress!,
                       ),
@@ -75,11 +111,15 @@ class HomeHeader extends StatelessWidget {
         ),
         IconButton(
           onPressed: () {
-            Navigator.of(context).push(
+            Navigator.of(context)
+                .push(
               MaterialPageRoute(
                 builder: (context) => const FavPlacesPage(),
               ),
-            );
+            )
+                .then((value) {
+              context.read<FavPlacesBloc>().add(FavPlacesRequested());
+            });
           },
           icon: const Icon(
             Icons.favorite,
