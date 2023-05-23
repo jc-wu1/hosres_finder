b9d58b2a09ad87b2797b17c5ae8f625107da5bb2
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2023-05-23 22:25:27 +0700

    remove prettydiologger

--- lib/core/network/dio_wrapper.dart
+++ lib/core/network/dio_wrapper.dart
@@ -1,13 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 Dio getDio() {
   Dio dio = Dio();
   dio
     ..options.connectTimeout = const Duration(seconds: 20)
     ..options.receiveTimeout = const Duration(seconds: 20);
-  dio.interceptors.add(
-    PrettyDioLogger(),
-  );
+
   return dio;
 }
